0a1165d49dba4e5e9caab36072f27fe94f60b15c
Erica Newman <iskanderun@users.noreply.github.com>
2021-08-31 14:07:34 -0500

    Update precip_function.R
    fixing multiple issues and creating automatic file pathways

--- aedes_precip/R_scripts/precip_function.R
+++ aedes_precip/R_scripts/precip_function.R
@@ -1,7 +1,7 @@
 # newmane@berkeley.edu and fengxiao@email.arizona.edu
 # match trap data with precipitation data
 # R software script
-# last updated: 2021.08.26
+# last updated: 2021.08.31
 
 # PART 1: Introduction
 # PART 2: Matching mosquito counts to local precipitation data
@@ -18,6 +18,10 @@
 # Precipitation’s complicated role in driving the abundance of an 
 # emerging disease vector in an urban, arid landscape
 
+# The function provided in part 2 (and example data files) provide a method for linking organismal data
+# with georeferenced information and dates to kriged precipitation data from the same area. The 
+# date range of the kriged data to be associated with each individual observation of organismal data 
+# can be specified by the user by modifying the code. Notes below.
 
 # Kriged layers of precipition data used in this study available for download at 
 # figshare.com at 
@@ -27,21 +31,33 @@
 # Note: these files are 12.5 MB as a .zip file, but will be larger than 9 GB 
 # once unzipped 
 
-# Where the code below indicates setting the directory to "~/.../", this indicates
-# that the user should point the code to wherever the data are stored, either locally 
-# or online. In some cases, both options are given and the user must choose which source to use. 
-# The file structure should follow that of "aedes_precip" in this github folder.
+# data for trap location information ("blurred" to protect currently operating 
+# machinery and personal addresses), and counts of male and female
+# Aedes aegypti collected at those traps, with collection dates
+# are available in the github repository.
+
+# The file structure used for these analyses follow that of the "aedes_precip" 
+# folder in this github repository.
+
+# The code below should automatically choose to run the code and get data from the correct folders,
+# however, the user may want to manually point the code to wherever the data are stored, either locally 
+# or online. Navigating to RStudio menu: Session > Set Working Directory > Choose Directory, 
+# and then selecting "Open" for the downloaded folder "maricopamosquitoes-master" will run
+# a line of code that sets the correct local directory once the github directory is downloaded.
+# Note that the data from Figshare will have to be downloaded and unzipped into the "rainfall_maricopa"
+# folder (see below), and the .dbf file in "R_data" will need to be unzipped after download.
 
 # A file structure that can be used if all of the data files are stored locally (i.e. 
-# on a user's computer,
-# github account, Dropbox folder or other cloud-based folder, or other pathway that 
-# can be accessed by the user directly)
+# on a user's computer, github account, Dropbox folder or other cloud-based folder,
+# or other pathway that can be accessed by the user directly)
 # is the following:
 
+# Project: Maricopa mosquitoes
 # > aedes_precip
 #    >> R_scripts
+#      ## precip_function.R (this script)
 #    >> model
-#         >>> rainfall_maricopa
+#         >>> rainfall_maricopa (complete files available on github)
 #             >>>> raw
 #               ## (files in this folder are named "5400_FOPR_17.xlsx", etc., 
 #                 and follow structure of the github folder)
@@ -49,25 +65,37 @@
 #               ## (files in this folder are named "X20150319_ped.tif", etc., 
 #                   and follow structure of the github folder)
 #         >>> R_data
-#            ## mos_info_prj.shp 
-#            ## df.csv
+#            ## occ_SPATIAL_proj.shp & associated files (see readme about unzipping large .dbf file)
+#            ## Ae_aegypti_mosqcounts_blurred.csv for direct inquiry
+#            ## df.csv used in Part 3 of this code as an output of Part 2
 
 ##########
 # PART 2: Matching mosquito counts to local precipitation data
 ##########
 
-#loading packages
+#loading packages; uncomment the installation code below if necessary
 
 # install.packages('raster')
 # install.packages('ggplot2')
+# install.packages('rstudioapi')
 library(raster)
 library(ggplot2)
+library(rstudioapi)
 
 
+# Getting the path of your current open file
+current_path = rstudioapi::getActiveDocumentContext()$path 
+setwd(dirname(current_path ))
+print( getwd() )
+# set path to aedes_precip
+setwd('../')
+print( getwd() )
+
+#############################
 # load mosquito data
+# make sure to unzip the .dbf file in this folder before running the next line
 
-occ <- shapefile("model/R_data/mos_info.shp")
-
+occ <- shapefile("/R_data/occ_SPATIAL_proj.shp")
 
 occ$trapDay <- as.Date(occ$labdate, "%m/%d/%Y")    #change date to better format
 
@@ -77,17 +105,19 @@ occ$trapDay <- as.Date(occ$labdate, "%m/%d/%Y")    #change date to better format
 occ$spp <- occ$A_gyp_F    # however, this line can be used instead 
     # to model only the female Ae. aegypti, as in the manuscript
 
-
+#############################
 # load PPT layers (these are available on FigShare)
 setwd("~/.../aedes_precip/")
-#setwd("~/Dropbox/aedes_precip/") #or set pathway to a user-created Dropbox folder, 
-#for example
+#setwd("~/Dropbox/aedes_precip/") #or set pathway to a user-created Dropbox folder, for example
 
 layers <- list.files("rainfall_maricopa/3_station_raster/",full.names = T,
                      pattern="_ped.tif$")
-
 layers
+one_ly <- raster(layers[2]) # view second layer loaded in list, as example
+plot(one_ly)
 
+#############################
+# Define the function calPPT
 # calPPT is a function to find PPT for a user-defined temporal window
 
 calPPT <- function(daysAgo, dayRange,ooo){
@@ -153,6 +183,7 @@ setwd()
 df <- data.frame(cbind(occ$spp,occ$trapDay,
                        ppt_data))
 
+
 ##########
 # PART 3: graphing the data
 ##########
@@ -162,20 +193,19 @@ df <- data.frame(cbind(occ$spp,occ$trapDay,
 # the above analysis as an example that can be graphed 
 # with the code below this line, or analysed with user-created code
 
-
+#############################
 # "df.csv" can be created above; or you can load "df.csv" directly by uncommenting 
 # the code below and running it
 
 setwd("aedes_precip")
 df <- read.csv("model/R_data/df.csv")
-
 head(df)
-names(df)[1:2] <- c("spp","trapDay")
 df <- reshape::melt(df,id=c("spp","trapDay"))
 head(df)
+#############################
 
 # subset data by abundance if applicable
-# test <- subset(df,spp<1500)
+test <- subset(df,spp<1500)
 
 #graph results
 ggplot(test,aes(value,spp)) +
